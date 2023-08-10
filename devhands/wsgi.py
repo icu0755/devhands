adc3e69e2e06f680032438feda6b26a063e0883c
Vladimir Ivanov <vladimir.ivanov@socialsweethearts.de>
2023-08-10 21:26:00 +0400

    initial routing

--- devhands/wsgi.py
+++ devhands/wsgi.py
@@ -13,4 +13,11 @@ from django.core.wsgi import get_wsgi_application
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'devhands.settings')
 
-application = get_wsgi_application()
+django_application = get_wsgi_application()
+
+
+def application(env, start_response):
+    if env['PATH_INFO'] == '/plain_wsgi':
+        start_response('200 OK', [('Content-Type', 'text/plain')])
+        return [b'Hello Plain WSGI']
+    return django_application(env, start_response)
